7acc80d027028a7051e1801dd2b56980056deabe
JohnyDeCoder <johnymora251@gmail.com>
2022-05-15 22:15:39 -0500

    Changed main form, method form and color themes

---
+++ defi-2022/Forms/Methods/DEFI - Integral Definida.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace defi_2022.Forms
+{
+    partial class DEFI_Integral_Definida
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DEFI_Integral_Definida
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 561);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DEFI_Integral_Definida";
+            this.Text = "DEFI - Integral Definida";
+            this.Load += new System.EventHandler(this.DEFI_Integral_Definida_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
